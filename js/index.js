048b4d4b61c37f07e9f60a983f250b9fe6b2cead
Ramiro Garcia <rg2607836@gmail.com>
2021-01-18 15:13:09 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+import countdown from "./modulos/count_down.js";
+import hideAlert from "./modulos/hide_alert_jobs.js";
+
+const d = document;
+
+d.addEventListener('DOMContentLoaded', e => {
+  e.preventDefault('');
+  countdown(
+    'countdown',
+    'May 3, 2021',
+    '¡Hoy es mi cumpleaños yeah!'
+  );
+  hideAlert()
+})
