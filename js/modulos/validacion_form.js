c36789e4bd5ab441f73d9ddc1fd244324e830bd9
Ramiro Garcia <rg2607836@gmail.com>
2021-01-31 20:22:09 -0300

    Interactuando con los inputs

--- js/modulos/validacion_form.js
+++ js/modulos/validacion_form.js
@@ -2,5 +2,13 @@ const d = document;
 
 export default function contactFormValidation() {
   const $form = d.querySelector('.form'),
-    $inputs = d.querySelectorAll('.form [required]')
+    $inputs = d.querySelectorAll('.form [required]');
+  console.log($inputs)
+
+  d.addEventListener('keyup', (e) => {
+    if (e.target.matches('.form [required]')) {
+      let $input = e.target,
+        pattern = $input.pattern || $input.dataset.pattern;
+    }
+  })
 }
