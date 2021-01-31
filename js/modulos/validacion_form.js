23a0eac2fc69e9fce345a66e0efbfc04d3e718cb
Ramiro Garcia <rg2607836@gmail.com>
2021-01-31 20:41:30 -0300

    Agregando los span de error dinamicamente

--- js/modulos/validacion_form.js
+++ js/modulos/validacion_form.js
@@ -3,7 +3,15 @@ const d = document;
 export default function contactFormValidation() {
   const $form = d.querySelector('.form'),
     $inputs = d.querySelectorAll('.form [required]');
-  console.log($inputs)
+  console.log($inputs);
+
+  $inputs.forEach((input) => {
+    const $span = d.createElement('span');
+    $span.id = input.name;
+    $span.textContent = input.title;
+    $span.classList.add('contact__form-error', 'none');
+    input.insertAdjacentElement('afterend', $span);
+  })
 
   d.addEventListener('keyup', (e) => {
     if (e.target.matches('.form [required]')) {
